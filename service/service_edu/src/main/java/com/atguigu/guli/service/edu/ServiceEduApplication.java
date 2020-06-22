91e204c6d199307605a0d8cb039a6ea65fa21c5d
lexinhu <jialna@qq.com>
2020-06-23 01:43:21 +0800

    Init

---
+++ service/service_edu/src/main/java/com/atguigu/guli/service/edu/ServiceEduApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.guli.service.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author 乐心湖
+ * @date 2020/6/23 1:25
+ **/
+@SpringBootApplication
+@ComponentScan({"com.atguigu.guli"})
+public class ServiceEduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEduApplication.class, args);
+    }
+}
