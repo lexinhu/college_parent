933e7794a346e7f42bb72f5bb08be32e1983597a
ZXH <jialna@qq.com>
2021-02-22 02:52:06 +0800

    部署测试

--- service/service_oss/src/main/java/com/xn2001/college/service/oss/ServiceOssApplication.java
+++ service/service_oss/src/main/java/com/xn2001/college/service/oss/ServiceOssApplication.java
@@ -2,6 +2,8 @@ package com.xn2001.college.service.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +12,8 @@ import org.springframework.context.annotation.ComponentScan;
  * @author 乐心湖
  * @date 2020/6/30 20:41
  **/
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
+//取消数据源自动配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,})
 @ComponentScan({"com.xn2001.college"})
 @EnableDiscoveryClient
 public class ServiceOssApplication {
--- service/service_vod/src/main/java/com/xn2001/college/service/vod/ServiceVodApplication.java
+++ service/service_vod/src/main/java/com/xn2001/college/service/vod/ServiceVodApplication.java
@@ -2,16 +2,19 @@ package com.xn2001.college.service.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 /**
  * @author 乐心湖
  * @date 2020/7/18 14:20
  **/
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //取消数据源自动配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan({"com.xn2001.college"})
 @EnableDiscoveryClient
 @EnableFeignClients
